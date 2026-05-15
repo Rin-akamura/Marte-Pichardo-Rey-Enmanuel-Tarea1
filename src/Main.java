25c2e5400809ff44ff933019c0aa3e233f3507c1
Rey Enmanuel Marte Picherdo <email>
2026-05-15 01:31:49 -0400

    Estructuracion del menu visual y seleccion de opcion para los usuarios

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===========================================");
+        System.out.println("          .:::MENU DE OPCIONES:::.         ");
+        System.out.println("===========================================");
+        System.out.println();
+
+        System.out.println("==========================================");
+        System.out.println("1.   .::CALCULADORA BASICA::.  ");
+        System.out.println("2.   .::NUMERO PAR O IMPAR::.  ");
+        System.out.println("3.   .::TABLA DE MULTIPLICAR::. ");
+        System.out.println("4.   .::CONTADOR DE VOCALES::. ");
+        System.out.println("5.   .::PROMEDIO DE NOTAS::.   ");
+        System.out.println("6.   .::CLASE ESTUDIANTE::.    ");
+        System.out.println("7.   .::CUENTA BANCARIA::.     ");
+        System.out.println("8.   .::HERENCIA DE VEHICULOS::.");
+        System.out.println("9.   .::MATRIZ 3x3::.    ");
+        System.out.println("10.  .::SISTEMA BASICO DE INVENTARIO::. ");
+        System.out.println("11.  .::SALIR DE LA APLICACION::.  ");
+        System.out.println("===========================================");
+        System.out.println();
+
+        System.out.print("Elija una opción: ");
+        int op = sc.nextInt();
+
+        do{
+            switch(op){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+            }
+        }while(op == 11);
+
+
+
+
+    }
+}
