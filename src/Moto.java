0fb96cb9ea31029312c08ef46fd1204c64d5f4f9
Rey Enmanuel Marte Picherdo <email>
2026-05-17 22:36:20 -0400

    Creacion de la clase hija Moto

---
+++ src/Moto.java
@@ -0,0 +1,73 @@
+public class Moto extends Vehiculo {
+
+    private String Tipo;
+    private String Cilindraje;
+    private String TipoManubrio;
+    private String TipoFreno;
+    private String Transmision;
+    private String TipoEscape;
+
+    public Moto() {
+
+    }
+
+    public Moto(String Tipo, String Cilindraje, String TipoManubrio, String TipoFreno, String Transmision, String TipoEscape, String marca, String modelo, String AFabricacion, String Color, String Kilometraje, String Placa, String Combustible, String Consumo){
+
+        super(marca, modelo, AFabricacion, Color, Kilometraje, Placa, Combustible, Consumo);
+
+        this.Tipo = Tipo;
+        this.Cilindraje = Cilindraje;
+        this.TipoManubrio = TipoManubrio;
+        this.TipoFreno = TipoFreno;
+        this.Transmision = Transmision;
+        this.TipoEscape = TipoEscape;
+    }
+
+    public void setTipo(String Tipo){
+        this.Tipo = Tipo;
+    }
+
+    public String getTipo(){
+        return Tipo;
+    }
+
+    public void setCilindraje(String Cilindraje){
+        this.Cilindraje = Cilindraje;
+    }
+
+    public String getCilindraje(){
+        return Cilindraje;
+    }
+
+    public void setTipoManubrio(String TipoManubrio){
+        this.TipoManubrio = TipoManubrio;
+    }
+
+    public String getTipoManubrio(){
+        return TipoManubrio;
+    }
+
+    public void setTipoFreno(String TipoFreno){
+        this.TipoFreno = TipoFreno;
+    }
+
+    public String getTipoFreno(){
+        return TipoFreno;
+    }
+
+    public void setTransmision(String Transmision){
+        this.Transmision = Transmision;
+    }
+
+    public String getTransmision(){
+        return Transmision;
+    }
+
+    public void setTipoEscape(String TipoEscape){
+        this.TipoEscape = TipoEscape;
+    }
+
+    public String getTipoEscape(){
+        return TipoEscape;
+    }
+}
